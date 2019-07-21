459f0ddf89321b3b5b4a7ec80a4d5469b26dcf39
lisilin013 <lisilin013@163.com>
2019-07-21 11:05:52 +0800

    ignore

---
+++ vio_hw/hw3/CurveFitting_LM/plot_lambda.py
@@ -0,0 +1,47 @@
+# coding:utf-8
+# 编码：
+# for inputting chinese
+
+# this is comment 行注释
+
+a  = 9
+
+'''
+segment comment like this
+this 
+is 
+comment 
+too
+'''
+
+# 使用type函数可以打印数据类型
+# print ('我爱你')
+# print (type('this is a str'))
+# print (type(3))
+# print (type(3.4))
+
+'''
+data type: 
+int float str  list
+1   2.3   'ds' [1,2] ['lisilin', 'bieshuxian']
+'''
+# list 列表
+ss = 4
+float_num = 2.3
+name_list = ['li', 'wang', 'shu', 'zhang']
+# print (name_list)
+# print (type(name_list))
+
+'''
+variable
+
+a = 4
+variable_name = data(any right kind of datatype)
+
+how to name a variable?
+dsafd 13223 __
+字母 number __
+
+
+element type in keyboard: wewewe 1213 !@#$&*)$*)_*@#_    
+'''
